721d0a2889de14d149cd1bd6ac58ce541f52c27b
CaiwuChen <96741036+CaiwuChen@users.noreply.github.com>
2025-03-26 12:46:43 -0400

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,8 +1,28 @@
 import requests
+import sys
 
-def perform_google_search(api_key, engine_id, query):
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={engine_id}&num={10}"
-    response = requests.get(url)
-    data = response.json()
-    urls = [item['link'] for item in data.get('items', [])]
-    return urls
+def google_search(query, API_KEY, Engine_ID, seen_urls):
+    num_results=10
+    url = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        "q": query,
+        "key": API_KEY,
+        "cx": Engine_ID,
+        "num": num_results
+    }
+   
+    response = requests.get(url, params=params)
+    results = response.json()
+
+    urls = []
+    
+    for item in results.get("items", []):
+        if item.get("link") not in seen_urls:
+            urls.append({
+                "URL": item.get("link")
+            })
+            seen_urls.add(item.get("link"))
+        else:
+            continue
+
+    return urls, seen_urls
